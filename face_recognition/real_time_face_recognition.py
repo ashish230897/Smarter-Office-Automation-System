7ecbc71361a02308ee3de6fb9955db4cd98e8de4
Ashish <ashish.agrawal2123@gmail.com>
2018-02-19 01:03:18 +0530

    face recognition

---
+++ face_recognition/real_time_face_recognition.py
@@ -0,0 +1,84 @@
+
+"""Performs face detection in realtime.
+   This file just need a Machine learning classifier weights file 
+   and A tensorflow file having the weights of the Deep Learning model to be used
+   both the dependencies can passed as an argument on the face.py file
+   It also requires MTCNN model pretrained weights in same directory
+    
+"""
+
+import argparse
+import sys
+import time
+
+import cv2
+
+import face
+
+
+def add_overlays(frame, faces, frame_rate):
+    if faces is not None:
+        for face in faces:
+            face_bb = face.bounding_box.astype(int)
+            cv2.rectangle(frame,
+                          (face_bb[0], face_bb[1]), (face_bb[2], face_bb[3]),
+                          (0, 255, 0), 2)
+            if face.name is not None:
+                cv2.putText(frame, face.name, (face_bb[0], face_bb[3]),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0),
+                            thickness=2, lineType=2)
+
+    
+
+
+def main(args):
+    frame_interval = 3  # Number of frames after which to run face detection
+    fps_display_interval = 5  # seconds
+    frame_rate = 0
+    frame_count = 0
+
+    video_capture = cv2.VideoCapture(0)
+    face_recognition = face.Recognition()
+    start_time = time.time()
+
+    if args.debug:
+        print("Debug enabled")
+        face.debug = True
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+        
+        if (frame_count % frame_interval) == 0:
+            faces = face_recognition.identify(frame)
+
+            # Check our current fps
+            end_time = time.time()
+            if (end_time - start_time) > fps_display_interval:
+                frame_rate = int(frame_count / (end_time - start_time))
+                start_time = time.time()
+                frame_count = 0
+
+        add_overlays(frame, faces, frame_rate)
+
+        frame_count += 1
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything is done, release the capture
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--debug', action='store_true',
+                        help='Enable some debug outputs.')
+    return parser.parse_args(argv)
+
+
+if __name__ == '__main__':
+    main(parse_arguments(sys.argv[1:]))
